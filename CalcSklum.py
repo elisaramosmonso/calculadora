f3f3dbc75a52b002690590b7be20cea8b1f54793
elisaramosmonso <98059527+elisaramosmonso@users.noreply.github.com>
2025-01-02 13:32:33 +0100

    Update CalcSklum.py

--- CalcSklum.py
+++ CalcSklum.py
@@ -254,29 +254,29 @@ if st.session_state.authenticated:
                     # Filtrar por el menor nivel de coincidencia para cada persona
                     df_filtrado = df_valoraciones_actualizadas.loc[df_valoraciones_actualizadas.groupby(['SUPERVISOR', 'NOMBRE', 'ÁREA', 'PUESTO'])['COINCIDENCIA_ORDEN'].idxmin()]
 
-                for _, row in df_filtrado.iterrows():
-                        nivel = row['COINCIDENCIA']
-                        nivel_g = nivel
-                        puesto = row['PUESTO']  # Asumimos que el puesto está en la fila
-                
-                        # t33 para los rangos retributivos
-                        bsresp = float(str(t33[(t33['PUESTO'] == puesto) & (t33['Nivel'] == nivel)]['Rango Retributivo'].iloc[0]).replace(',', '.'))
-                        bsger = float(str(t33[(t33['PUESTO'] == puesto) & (t33['Nivel'] == nivel_g)]['Rango Retributivo'].iloc[0]).replace(',', '.'))
-                        propret = 0.5 * (bsresp + bsger)
-                
-                        # Añadir el resultado para esta persona
-                        df_resultados.append({
-                            'Supervisor': row['SUPERVISOR'],
-                            'NOMBRE': row['NOMBRE'],
-                            'PUESTO': row['PUESTO'],
-                            'PROPRET': propret,
-                            "FECHA": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                        })
-                
-                    # Crear DataFrame con los resultados de las retribuciones
-                df_resultados = pd.DataFrame(df_resultados)
-                insertar_valoraciones_en_sql(df_valoraciones_actualizadas)
-                insertar_resultados_en_sql(df_resultados)
+                    for _, row in df_filtrado.iterrows():
+                            nivel = row['COINCIDENCIA']
+                            nivel_g = nivel
+                            puesto = row['PUESTO']  # Asumimos que el puesto está en la fila
+                    
+                            # t33 para los rangos retributivos
+                            bsresp = float(str(t33[(t33['PUESTO'] == puesto) & (t33['Nivel'] == nivel)]['Rango Retributivo'].iloc[0]).replace(',', '.'))
+                            bsger = float(str(t33[(t33['PUESTO'] == puesto) & (t33['Nivel'] == nivel_g)]['Rango Retributivo'].iloc[0]).replace(',', '.'))
+                            propret = 0.5 * (bsresp + bsger)
+                    
+                            # Añadir el resultado para esta persona
+                            df_resultados.append({
+                                'Supervisor': row['SUPERVISOR'],
+                                'NOMBRE': row['NOMBRE'],
+                                'PUESTO': row['PUESTO'],
+                                'PROPRET': propret,
+                                "FECHA": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                            })
+                    
+                        # Crear DataFrame con los resultados de las retribuciones
+                    df_resultados = pd.DataFrame(df_resultados)
+                    insertar_valoraciones_en_sql(df_valoraciones_actualizadas)
+                    insertar_resultados_en_sql(df_resultados)
                     
             else:
                 st.warning(f"No hay preguntas para el área **{area_persona}** y puesto **{puesto_persona}**.")
