8c98fe954b48364ce7f02775ef25f34ba29869e1
elisaramosmonso <98059527+elisaramosmonso@users.noreply.github.com>
2025-01-08 18:14:11 +0100

    Update CalcSklum.py

--- CalcSklum.py
+++ CalcSklum.py
@@ -244,7 +244,7 @@ if st.session_state.authenticated:
                         
                     tprueb2['diferencia'] = abs(tprueb2['suma_valoraciones'] - valoracion)   
                     nivel = tprueb2.loc[tprueb2['diferencia'].idxmin()]
-                    st.table(nivel)
+                    st.write(nivel)                    
                     puesto = row['PUESTO']  
                     bsresp = float(str(t33[(t33['PUESTO'] == puesto) & (t33['Nivel'] == nivel)]['Rango Retributivo'].iloc[0]).replace(',', '.'))
                     propret = bsresp
