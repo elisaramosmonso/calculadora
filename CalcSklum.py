33f0fbd9dd8b8900e2d90b6ae8ba9588560e31f3
elisaramosmonso <98059527+elisaramosmonso@users.noreply.github.com>
2025-01-08 18:26:15 +0100

    Update CalcSklum.py

--- CalcSklum.py
+++ CalcSklum.py
@@ -248,8 +248,7 @@ if st.session_state.authenticated:
                     
                     bsresp = float(str(t33[(t33['PUESTO'] == puesto) & (t33['Nivel'] == nivel)]['Rango Retributivo'].iloc[0]).replace(',', '.'))
                     propret = bsresp
-                    df_resultados.append({
-                        'Supervisor': row['SUPERVISOR'],
+                    df_resultados.append({'Supervisor': row['SUPERVISOR'],
                         'NOMBRE': row['NOMBRE'],
                         'PUESTO': row['PUESTO'],
                         'PROPRET': propret,
